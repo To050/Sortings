9b3d506eea4610dcf69a3870669f29ac017b5336
To050 <149853520+To050@users.noreply.github.com>
2024-06-16 20:36:26 +0700

    selection, counting, radix sort

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 
 #include "utils.h"
 #include "quick_sort.h"
+#include "selection_sort.h"
+#include "counting_sort.h"
+#include "radix_sort.h"
 #include "data_generator.h"
 
 using namespace std;
@@ -16,7 +19,7 @@ int main(int argc, char **argv) {
     ofstream out("output.txt");
 
     int *a = new int[500000];
-    GenerateRandomData(a, 10000);
+    GenerateReverseData(a, 10000);
     int ns[] = {100, 1000, 5000, 10000, 50000, 500000};
     for (int i = 0; i < 6; ++i) {
         int n = ns[i];
@@ -25,7 +28,7 @@ int main(int argc, char **argv) {
             b[j] = a[j];
         }
 
-        RunInfo run = benchmark(b, n, quickSortComparisions);
+        RunInfo run = benchmark(b, n, countingSort);
         out << run.elapsed_time << "\n";
         delete[] b;
     }
